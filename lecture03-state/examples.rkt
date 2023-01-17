0a07bc4787b24ef68d8b1c587f0aecaa9a284763
Geoffrey Mainland <mainland@apeiron.net>
2023-01-17 09:06:58 -0500

    Update lecture 3 examples

--- lecture03-state/examples.rkt
+++ lecture03-state/examples.rkt
@@ -1,19 +1,36 @@
 #lang racket
 (require racket/trace)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Higher Order Function Examples
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;
-; Define a function map that takes a function f and a list xs and returns a new
-; list consisting of the results of applying f to each element in xs.
+; Calculate the squares of a list of integers. Make the function non-recursive.
 ;
 ; Example:
-;  (map (lambda (x) (+ x 1)) '(1 2 3)) => '(2 3 4)
+;   (squares '(1 2 3 4 5)) => '(1 4 9 16 25)
 ;
 
-(define (map f lst)
+(define (squares xs)
+  'not-implemented)
+
+;
+; Calculate the sum of squares of a list of integers. Make the function non-recursive.
+;
+; Example:
+;   (sum-of-squares '(1 2 3 4 5)) => 55
+;
+
+(define (sum-of-squares xs)
+  'not-implemented)
+
+;
+; Return a list containing only the elements of xs for which the predicate f
+; returns true.
+;
+; Examples:
+;   (filter (lambda (x) (> x 0)) '(-1 -4 5 6 0)) => '(5 6)
+;
+
+(define (filter f xs)
   'not-implemented)
 
 ;
@@ -27,56 +44,25 @@
 (define (reduce f z xs)
   'not-implemented)
 
-; now try it the other way...
-
-;
-; Return a list containing only the elements of xs for which the predicate f
-; returns true.
-;
-; Examples:
-;   (filter (lambda (x) (> x 0)) '(-1 -4 5 6 0)) => '(5 6)
-;
-
-(define (filter f xs)
-  'not-implemented)
-      
-;
-; Compute the sum of a list of integers.
-;
-; Example:
-;   (sum '(1 2 3 4)) => 10
-
-;; Write a solution in terms of reduce
-
-(define (sum xs)
-  'not-implemented)
-
-;
-; Calculate the sum of squares of a list of integers. Make the function non-recursive.
-;
-; Example:
-;   (sum-of-squares '(1 2 3 4 5)) => 55
-;
-
-(define (sum-of-squares xs)
-  'not-implemented)
-
 ; Compute the order of an object (maximum depth)
 ;  The order of an atom is 0.
 ;  The order of a list is 1 plus the maximum order of its elements.
 ;
 ; Example:
 ;   (order '(1 (2 (3)) 4)) => 3
-;   (order '((()))) => 3
+;   (order '((()))) => 2
 ;
 ; How can we test for an atom?
 
-(define (order l)
+(define (order xs)
   'not-implemented)
 
 ;
 ; Write a non-recursive function that takes a list and returns all the even
 ; integers in the list
+;
+; Examples:
+;   (only-even '(1 2 3 4 5)) => '(2 4)
 
 (define (only-even xs)
   'not-implemented)
@@ -92,6 +78,30 @@
 
 (define (papply f x)
   'not-implemented)
+
+; Add one to a number
+; Define using papply
+
+(define inc
+  'not-implemented)
+
+;;
+;; Folds
+;;
+
+; Right fold
+
+(define (foldr f z xs)
+  'not-implemented)
+
+;; (show-tree (foldr (show-function 'cons) 'null '(1 2 3 4)))
+
+; Left fold
+
+(define (foldl f z xs)
+  'not-implemented)
+
+;; (show-tree (foldl (show-function 'cons) 'null '(1 2 3 4)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; State Examples
